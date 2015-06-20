c3c7d4fc99ae361770542a300086a6f917621033
Larry Garfield <larry@garfieldtech.com>
2015-06-19 22:58:36 -0500

    Add immutable modifiers (which need a better name) for links.

--- src/Link.php
+++ src/Link.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: crell
- * Date: 6/19/15
- * Time: 10:49 PM
- */
 
 namespace Crell\HtmlModel;
 
@@ -13,6 +7,31 @@ class Link implements LinkInterface
 {
     use LinkTrait;
 
+    public function __construct($href, $rel)
+    {
+        $this->href = $href;
+        $this->rel = $rel;
+    }
 
+    public function withHref($href)
+    {
+        $that = clone($this);
+        $that->href = $href;
+        return $that;
+    }
+
+    public function withRel($rel)
+    {
+        $that = clone($this);
+        $that->rel = $rel;
+        return $that;
+    }
+
+    public function withAttribute($key, $value)
+    {
+        $that = clone($this);
+        $that->attributes[$key] = $value;
+        return $that;
+    }
 
 }
