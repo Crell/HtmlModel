19530ee4e46c7bf6da108542f6bf3792e222b6b8
Larry Garfield <larry@garfieldtech.com>
2015-06-19 22:53:00 -0500

    Create basic Link tools per FIG discussion.

---
+++ src/Link.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: crell
+ * Date: 6/19/15
+ * Time: 10:49 PM
+ */
+
+namespace Crell\HtmlModel;
+
+
+class Link implements LinkInterface
+{
+    use LinkTrait;
+
+
+
+}
---
+++ src/LinkInterface.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Crell\HtmlModel;
+
+interface LinkInterface
+{
+
+    /**
+     * Returns the target of the link.
+     *
+     * The target must be a URI or a Relative URI reference.
+     *
+     * @return string
+     */
+    public function getHref();
+
+    /**
+     * Returns the relationship type(s) of the link.
+     *
+     * This method returns 0 or more relationship types for a link, expressed
+     * as an array of strings.
+     *
+     * The returned values should be either a simple keyword or an absolute
+     * URI. In case a simple keyword is used, it should match one from the
+     * IANA registry at:
+     *
+     * http://www.iana.org/assignments/link-relations/link-relations.xhtml
+     *
+     * Optionally the microformats.org registry may be used, but this may not
+     * be valid in every context:
+     *
+     * http://microformats.org/wiki/existing-rel-values
+     *
+     * Private relationship types should always be an absolute URI.
+     *
+     * @return string[]
+     */
+    public function getRel();
+
+    /**
+     * Returns a list of attributes that describe the target URI.
+     *
+     * The list should be specified as a key-value list.
+     *
+     * There is no formal registry of the values that are allowed here, and
+     * validity of values is depdendent on context.
+     *
+     * Common values are 'hreflang', 'title', and 'type'. Implementors
+     * embedding a serialized version of a link are responsible for only
+     * encoding the values they support.
+     *
+     * Any value that appears that is not valid in the context in which it is
+     * used should be ignored.
+     *
+     * Some attributes, (commonly hreflang) may appear more than once in their
+     * context. Attributes such as those may be specified as an array of
+     * strings.
+     *
+     * @return array
+     */
+    public function getAttributes();
+
+}
