991c82dcf0796a2b0928e6aa180a3271d948fd24
Larry Garfield <larry@garfieldtech.com>
2015-06-21 23:12:30 -0500

    Add tests for StyleLinkElement.

---
+++ tests/Head/StyleLinkElementTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Crell\HtmlModel\Test\Head;
+
+
+use Crell\HtmlModel\Head\StyleLinkElement;
+
+class StyleLinkElementTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        $link = new StyleLinkElement('styles.css');
+
+        $this->assertEquals('stylesheet', $link->getRel());
+        $this->assertEquals('styles.css', $link->getHref());
+    }
+
+}
