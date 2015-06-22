f682a5385ce170e04bbd2ee03c2950da1c903862
Larry Garfield <larry@garfieldtech.com>
2015-06-21 23:09:44 -0500

    Use LinkModifiersTrait in LinkElement to reduce duplication.

--- src/Head/LinkElement.php
+++ src/Head/LinkElement.php
@@ -3,10 +3,13 @@
 namespace Crell\HtmlModel\Head;
 
 use Crell\HtmlModel\Link\LinkInterface;
+use Crell\HtmlModel\Link\LinkModifiersTrait;
 use Crell\HtmlModel\Link\ModifiableLinkInterface;
 
 class LinkElement extends HeadElement implements LinkInterface, ModifiableLinkInterface
 {
+    use LinkModifiersTrait;
+
     protected $element = 'link';
 
     public function __construct($rel, $href)
@@ -33,32 +36,6 @@ class LinkElement extends HeadElement implements LinkInterface, ModifiableLinkIn
     public function getRel()
     {
         return $this->getAttribute('rel');
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function withHref($href)
-    {
-        return $this->withAttribute('href', $href);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function withRel($rel)
-    {
-        return $this->withAttribute('rel', $rel);
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * Force this method public to comply with ModifiableLinkInterface
-     */
-    public function withAttribute($key, $value)
-    {
-        return parent::withAttribute($key, $value);
     }
 
 }
