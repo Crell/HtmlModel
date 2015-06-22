a9f957b326b841832aed1edade21c287a74d0074
Larry Garfield <larry@garfieldtech.com>
2015-06-21 22:57:34 -0500

    Add an internal setter to Content trait.

--- src/ContentTrait.php
+++ src/ContentTrait.php
@@ -28,12 +28,26 @@ trait ContentTrait
      *
      * @param string $content
      *   The body of the page to set, that is, everything inside the <body> tag.
-     * @return static
+     * @return self
      */
     public function withContent($content)
     {
         $that = clone($this);
         $that->content = $content;
         return $that;
+    }
+
+    /**
+     * Sets the content of this element.
+     *
+     * Note: This method is only to be called from the constructor. Calling it
+     * at any other time is an error as it would violate immutability.
+     *
+     * @param string $content
+     *   The content to set.
+     */
+    protected function setContent($content)
+    {
+        $this->content = $content;
     }
 }
