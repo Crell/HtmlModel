013c277f3652005df8002edd7d63769cd38b54ae
Larry Garfield <larry@garfieldtech.com>
2015-07-11 18:22:29 -0500

    Improve docs.

--- src/HtmlFragment.php
+++ src/HtmlFragment.php
@@ -6,6 +6,14 @@ use Crell\HtmlModel\Head\HeadElement;
 use Crell\HtmlModel\Link\Linkable;
 use Crell\HtmlModel\Link\LinkInterface;
 
+/**
+ * Value object representing a portion of an HTML page.
+ *
+ * An HtmlFragment is useful to model a subset of a page, such as sidebar
+ * elements or similar. The idea is that it can carry not just the string
+ * but also associated metadata, like what style elements are necessary for
+ * that markup.
+ */
 class HtmlFragment implements HtmlFragmentInterface, Linkable
 {
     use ContentElementTrait;
