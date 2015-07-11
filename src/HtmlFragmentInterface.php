3451431a77a21919753a307f7e46aa5a6a61cca9
Larry Garfield <larry@garfieldtech.com>
2015-07-11 18:57:52 -0500

    Fix generated doc and add dependency to HtmlFragmentInterface.

--- src/HtmlFragmentInterface.php
+++ src/HtmlFragmentInterface.php
@@ -1,13 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: crell
- * Date: 6/19/15
- * Time: 11:04 PM
- */
 
 namespace Crell\HtmlModel;
 
-interface HtmlFragmentInterface extends StyleContainerInterface, ScriptContainerInterface, ContentElementInterface
+interface HtmlFragmentInterface extends StyleContainerInterface, ScriptContainerInterface, ContentElementInterface, HeadElementContainerInterface
 {
 }
