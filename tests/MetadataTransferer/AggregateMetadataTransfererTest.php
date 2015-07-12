c5fffd0077e94893d061136f44f5ac4a59fe7fcb
Larry Garfield <larry@garfieldtech.com>
2015-07-11 20:11:55 -0500

    Beef up Aggregate transferer test, and make it PHP 5.4 compatible, just to be nice.

--- tests/MetadataTransferer/AggregateMetadataTransfererTest.php
+++ tests/MetadataTransferer/AggregateMetadataTransfererTest.php
@@ -2,51 +2,64 @@
 
 namespace Crell\HtmlModel\Test\MetadataTransferer;
 
-use Crell\HtmlModel\Head\LinkElement;
-use Crell\HtmlModel\Head\MetaElement;
 use Crell\HtmlModel\Head\ScriptElement;
 use Crell\HtmlModel\Head\StyleElement;
 use Crell\HtmlModel\Head\StyleLinkElement;
 use Crell\HtmlModel\HtmlFragment;
 use Crell\HtmlModel\MetadataTransfer\AggregateMetadataTransferer;
-use Crell\HtmlModel\MetadataTransfer\StyleTransferer;
+use Crell\HtmlModel\ScriptContainerInterface;
 use Crell\HtmlModel\StyleContainerInterface;
-use Crell\HtmlModel\MetadataTransfer\MetadataTransfererInterface;
 use Prophecy\Argument;
-use Prophecy\Prophecy\MethodProphecy;
 
 class AggregateMetadataTransfererTest extends \PHPUnit_Framework_TestCase
 {
 
     public function testAggregateHandoff()
     {
-        $sub1 = $this->prophesize(StyleTransferer::class);
+        $sub1 = $this->prophesize('Crell\HtmlModel\MetadataTransfer\StyleTransferer');
         $sub1
-          ->transfer(Argument::type(StyleContainerInterface::class), Argument::type(StyleContainerInterface::class))
+          ->transfer(Argument::type('Crell\HtmlModel\StyleContainerInterface'), Argument::type('Crell\HtmlModel\StyleContainerInterface'))
           ->will(function ($args) {
-            return new HtmlFragment();
+              return new HtmlFragment();
           })
           ->shouldBeCalled();
+
+        $sub2 = $this->prophesize('Crell\HtmlModel\MetadataTransfer\ScriptTransferer');
+        $sub2
+          ->transfer(Argument::type('Crell\HtmlModel\ScriptContainerInterface'), Argument::type('Crell\HtmlModel\ScriptContainerInterface'))
+          ->will(function ($args) {
+              return new HtmlFragment();
+          })
+          ->shouldBeCalled();
+
+        // HtmlFragment doesn't implement StatusCodeContainerInterface, so
+        // this transferer should be skipped entirely.
+        $sub3 = $this->prophesize('Crell\HtmlModel\MetadataTransfer\StatusCodeTransferer');
+        $sub3
+          ->transfer(Argument::type('Crell\HtmlModel\StatusCodeContainerInterface'), Argument::type('Crell\HtmlModel\StatusCodeContainerInterface'))
+          ->shouldNotBeCalled();
 
         $src = new HtmlFragment();
 
         /** @var HtmlFragment $src */
         $src = $src
-          //->withScript(new ScriptElement('header.js'))
-          //->withScript(new ScriptElement('footer.js'), 'footer')
+          ->withScript(new ScriptElement('header.js'))
+          ->withScript(new ScriptElement('footer.js'), 'footer')
           ->withStyleLink(new StyleLinkElement('css.css'))
           ->withInlineStyle(new StyleElement('CSS here'))
-          //->withContent('Body here')
         ;
 
         $dest = new HtmlFragment();
 
         $transferer = new AggregateMetadataTransferer([
-          StyleContainerInterface::class => $sub1->reveal(),
+          'Crell\HtmlModel\StyleContainerInterface' => $sub1->reveal(),
+          'Crell\HtmlModel\ScriptContainerInterface' => $sub2->reveal(),
+          'Crell\HtmlModel\StatusCodeContainerInterface' => $sub3->reveal(),
         ]);
 
         // The mocks above will ensure that everything gets called that should
         // get called.
         $transferer->transfer($src, $dest);
     }
+
 }
