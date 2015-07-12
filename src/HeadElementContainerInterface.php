23bac41f8ebe0c64b38c9d2b59697ab61467806d
Larry Garfield <larry@garfieldtech.com>
2015-07-11 19:55:51 -0500

    Fill in HeadElementInterface for reals this time.

--- src/HeadElementContainerInterface.php
+++ src/HeadElementContainerInterface.php
@@ -2,6 +2,27 @@
 
 namespace Crell\HtmlModel;
 
+use Crell\HtmlModel\Head\HeadElement;
+
 interface HeadElementContainerInterface
 {
+    /**
+     * Returns a copy of the page with the provided HeadElement added.
+     *
+     * @param HeadElement $element
+     *   The HeadElement to add.
+     *
+     * @return static
+     */
+    public function withHeadElement(HeadElement $element);
+
+    /**
+     * Returns an array of all Head elements on this object.
+     *
+     * Remember that some head-scoped elements may have their own alternate
+     * colletions and won't be included here.
+     *
+     * @return HeadElement[]
+     */
+    public function getHeadElements();
 }
--- src/HtmlPageInterface.php
+++ src/HtmlPageInterface.php
@@ -75,26 +75,6 @@ interface HtmlPageInterface extends ContentElementInterface, StatusCodeContainer
     public function withBodyAttribute($key, $value);
 
     /**
-     * Returns a copy of the page with the provided HeadElement added.
-     *
-     * @param HeadElement $element
-     *   The HeadElement to add.
-     *
-     * @return static
-     */
-    public function withHeadElement(HeadElement $element);
-
-    /**
-     * Returns an array of all Head elements on this object.
-     *
-     * Remember that some head-scoped elements may have their own alternate
-     * colletions and won't be included here.
-     *
-     * @return HeadElement[]
-     */
-    public function getHeadElements();
-
-    /**
      * Returns the title of the page.
      *
      * @return string
